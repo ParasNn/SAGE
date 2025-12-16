c5ece12c4e8a2041c2dccf6b1b59dd6db565366d
ParasNn <40841618+ParasNn@users.noreply.github.com>
2025-12-16 15:24:29 -0600

    Java + Sring Boot init

---
+++ sage-backend/src/test/java/com/SAGE/sageWebsite/SageWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SAGE.sageWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SageWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
